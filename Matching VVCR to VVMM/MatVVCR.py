9e51986a97aefe23f4ddf5634d0fa610c8fdf18c
AdamRauff <adam.rauff@ucdenver.edu>
2017-03-27 13:40:06 -0600

    added few lines to read in Outcomes csv file containing VVCR values

--- Matching VVCR to VVMM/MatVVCR.py
+++ Matching VVCR to VVMM/MatVVCR.py
@@ -8,8 +8,12 @@
 import csv
 
 # read in the VVMM database from the 2 csv files
+# since script is only used once, these names are hard coded
+# should expand code to provide command line input or 
+# windows explorer window to allow user to interactively choose files
 DBname = 'VVMM_Database.csv' 
 demoName = 'VVMM_demographics.csv'
+OutCname = ''
 
 # list that hold to the patient numbers from the VVMM Database csv file
 PatNum = []
@@ -66,3 +70,46 @@ for i, row in enumerate(Data):
       demDOB.append(Data[i][j])
 
 VVMM_Demo_List = [demPtNo, demFstNam, demLstNam, demGender, demMRN, demDOB]
+
+# ------------------------------------------------------------------------------------
+# read in the csv of the outcomes that contains VVCR information 
+# calculated from pressure data
+
+# declare lists of the columns to store from the outcomes file
+# these variables will be compared to the values obtained from the VVMM database
+# in order to find matches and provide the VVCR values
+KH_VVCR = []
+UT_VVCR = []
+OUT_MRN = []
+OUT_DOB = []
+OUT_Fnam = []
+Out_Lnam = []
+
+with open(OutCname, 'r') as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+
+# store all data of csv in a list of lists
+  Data = list(csv_reader)
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# column values have not been validated!!
+# see csv file of outcomes to set the correct j value for each list
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+for i, row in enumerate(Data):
+  for j, column in enumerate(row):
+    if j == 0 and i > 0:
+      OUT_MRN.append(Data[i][j])
+    if j == 1 and i > 0:
+      OUT_DOB.append(Data[i][j])
+    if j == 3 and i > 0:
+      OUT_Fnam.append(Data[i][j])
+    if j == 4 and i > 0:
+      Out_Lnam.append(Data[i][j])
+    if j == 6 and i > 0:
+      UT_VVCR.append(Data[i][j])
+    if j == 5 and i > 0:
+      KH_VVCR.append(Data[i][j])
+
+# ----------------------------------------------------------------------------
+# compare MRN, names, DOB values of Outcomes and VVMM database to see 
+# if any of the same patients exists in both
